4682a2055d4071fdd3b8373aeb3e91ed57df8fb1
Rob97 <sohrabseera@hotmail.com>
2019-04-03 16:09:41 -0700

    http request added

--- app/src/main/java/com/example/sohrabseera/myapplicationjava/MainActivity.java
+++ app/src/main/java/com/example/sohrabseera/myapplicationjava/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -12,6 +13,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -19,6 +22,7 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
     private Button scanBtn;
+    private Button nameBtn;
     private TextView formatTxt, contentTxt;
     private String scanContent;
 
@@ -27,23 +31,39 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        nameBtn = (Button)findViewById(R.id.name_button);
         scanBtn = (Button)findViewById(R.id.scan_button);
         formatTxt = (TextView)findViewById(R.id.scan_format);
         contentTxt = (TextView)findViewById(R.id.scan_content);
 
         scanBtn.setOnClickListener(this);
+        nameBtn.setOnClickListener(this);
     }
 
+    @Override
     public void onClick(View v){
+
         if(v.getId()==R.id.scan_button){
             IntentIntegrator scanIntegrator = new IntentIntegrator(this);
             scanIntegrator.initiateScan();
         }
+        if(v.getId()==R.id.name_button) {
+                try {
+                    Log.e("Entered Click", "Entered Click");
+                    getItemName();
+                } catch (Exception e) {
+                    System.out.println("Exception occurred on click");
+                }
+            }
+
+
     }
+
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
         if (scanningResult != null) {
+
             scanContent = scanningResult.getContents();
             String scanFormat = scanningResult.getFormatName();
             formatTxt.setText("FORMAT: " + scanFormat);
@@ -57,16 +77,48 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     }
 
 
+public void getItemName()throws IOException {
+    try {
+        Log.e("Entered Scan", "Entered Scan");
+        //Debugging
+        URL url = new URL("https://api.upcitemdb.com/prod/trial/lookup?upc=674785680773");
+      //  URL url = new URL("https://api.upcitemdb.com/prod/trial/lookup?upc=" + scanContent);
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        Log.e("Test", "Test");
+        try {
+            Log.e("Test1", "Test1");
+            try {
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
 
-
-    URL url = new URL("http://www.android.com/");
-    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-   try {
-        InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-        readStream(in);
-    } finally {
-        urlConnection.disconnect();
+                Log.e("Returned Scan", "Returned Scan");
+                readStream(in);
+            }catch(Exception e){
+                Log.e("failed", "failed");
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }catch(Exception e) {
+        System.out.println("Exception occurred");
     }
+}
+    private String readStream(InputStream is) {
+        try {
+            Log.e("Entered output", "Entered Output");
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+            int i = is.read();
+            while(i != -1) {
+                bo.write(i);
+                i = is.read();
+            }
+            return bo.toString();
+        } catch (IOException e) {
+            Log.e("Entered Error on Output", "Entered Error on Output");
+            return "Errorrrr";
+        }
+    }
+
+
 
 
 
